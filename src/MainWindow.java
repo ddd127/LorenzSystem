c9f2a303206cf47a60fe39f936e889635fdf041d
Danil Demintsev <demintsievd@yandex.ru>
2020-11-29 19:00:49 +0300

    Add MinimumSize for window

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -9,26 +9,13 @@ public class MainWindow extends JFrame {
         Color background = new Color(50, 50, 50);
 
         this.setSize(1100, 700);
-        this.setPreferredSize(new Dimension(1100, 837));
+        this.setMinimumSize(new Dimension(900, 700));
         this.setResizable(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().setBackground(background);
 
         int settingsWidth = 300;
         this.add(new View(line, settingsWidth), BorderLayout.WEST);
-//        JScrollPane scrollPane = new JScrollPane(new SettingsPanel(model));
-//        scrollPane.setBackground(background);
-//        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-//        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-//        scrollPane.getVerticalScrollBar().setBackground(background);
-//        scrollPane.getViewport().setBackground(background);
-//        scrollPane.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
-//            @Override
-//            protected void configureScrollBarColors() {
-//                this.thumbColor = Color.BLACK;
-//            }
-//        });
-//        this.add(scrollPane, BorderLayout.EAST);
         this.add(new SettingsPanel(model), BorderLayout.EAST);
         this.setVisible(true);
     }
